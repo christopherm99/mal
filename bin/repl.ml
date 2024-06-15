df90e113e6d6696f8b159d47c61e2513c66ea86a
Christopher Milan <chrismilan@ucla.edu>
2024-06-15 15:20:07 -0700

    multiline works

--- bin/repl.ml
+++ bin/repl.ml
@@ -1,36 +1,29 @@
 open Mal.Parser.MenhirInterpreter
 
-(* let rec repl_multiline prev_input =
-     print_string "... ";
-     try
-       let input = prev_input ^ read_line () in
-       try
-         let program = Mal.Utils.parse_program input in
-         ignore @@ List.map Mal.Codegen.codegen_statement program;
-         Llvm.dump_module Mal.Codegen.the_module;
-         repl ()
-       with
-       | Mal.Utils.ParseError pos ->
-         if pos.pos_lnum = 1 then repl_multiline input
-         else
-           Printf.printf "ParseError at \"%s\" line %d column %d\n" pos.pos_fname
-             pos.pos_lnum pos.pos_bol;
-         repl ()
-       | Mal.Utils.LexError (pos, msg) ->
-           Printf.printf "LexError(%s) at \"%s\" line %d column %d\n" msg pos.pos_fname pos.pos_lnum pos.pos_bol;
-           repl ()
-     with End_of_file ->
-       print_string "Input Cancelled\n";
-       repl ()
-   and *)
 let rec repl () =
   print_string "> ";
   try
-    let input = read_line () in
-    let lb = Lexing.from_string input in
-    let rec helper chkpt lb =
+    let rec multiline_helper lb =
+      print_string "... ";
+      let input = read_line () in
+      let buf =
+        Bytes.cat lb.Lexing.lex_buffer (Bytes.of_string ("\n  " ^ input))
+      in
+      {
+        lb with
+        Lexing.lex_buffer = buf;
+        Lexing.lex_buffer_len = Bytes.length buf;
+      }
+    and helper chkpt lb =
       match chkpt with
       | InputNeeded _ ->
+          let lb =
+            if
+              lb.Lexing.lex_curr_pos == lb.Lexing.lex_buffer_len
+              && not (acceptable chkpt Mal.Parser.EOF lb.Lexing.lex_curr_p)
+            then multiline_helper lb
+            else lb
+          in
           helper
             (offer chkpt
                ( Mal.Lexer.token lb,
@@ -39,12 +32,12 @@ let rec repl () =
             lb
       | Shifting (_, _, _) -> helper (resume chkpt) lb
       | AboutToReduce (_, _) -> helper (resume chkpt) lb
-      | HandlingError _ ->
-          print_string "handling error";
-          helper (resume chkpt) lb
+      | HandlingError _ -> helper (resume chkpt) lb
       | Accepted prg -> prg
       | Rejected -> raise (Mal.Utils.ParseError (Lexing.lexeme_start_p lb))
     in
+    let lb = Lexing.from_string (read_line ()) in
+    Lexing.set_filename lb "<stdin>";
     let program = helper (Mal.Parser.Incremental.toplevel lb.lex_start_p) lb in
     Format.printf "%a" (Format.pp_print_list Mal.Pp.pp_stmt) program;
     Format.print_newline ();
@@ -54,8 +47,6 @@ let rec repl () =
   with
   | End_of_file -> exit 0
   | Mal.Utils.ParseError pos ->
-      (*if pos.pos_lnum = 1 then repl_multiline input
-        else*)
       Printf.printf "ParseError at \"%s\" line %d column %d\n" pos.pos_fname
         pos.pos_lnum pos.pos_bol;
       repl ()
